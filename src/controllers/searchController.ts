94ad4bceebc791dec318c6f8d94f1cebd3575ffc
Lucas Cavalcanti de Araujo <lcavalcantidearaujo@gmail.com>
2022-08-25 16:06:18 -0300

    final project 1.0

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,21 @@
 import {Request, Response} from "express";
 
-export const seacrh = (req: Request, res: Response) => {
+import {Pet} from "../models/pet";
+import {createMenuObject} from "../helpers/createMenuObject";
 
+export const seacrh = (req: Request, res: Response) => {
+    let query: string = req.query.q as string;
+
+    if (!query) {
+        res.redirect("/");   //se não tiver digitado nada no query, redireciona para página inicial
+        return; 
+    }
+
+    let list = Pet.getFromName(query);
+    
+    res.render("pages/page.mustache", {
+        menu: createMenuObject(""),
+        list,
+        query
+    })
 }
